438c89a85f9ea42a19364743f9fdea328e33a9ff
CryptoGraffe <graffe@graffe.co>
2022-02-11 00:45:31 -0800

    Adding useTypedMutation and mutation for updating avatar

--- src/hooks/gql.ts
+++ src/hooks/gql.ts
@@ -5,8 +5,12 @@ import {
   OperationOptions,
   chainOptions,
 } from 'lib/gql/zeusUser';
-import { useTypedQuery as _useTypedQuery } from 'lib/gql/zeusUser/reactQuery';
+import {
+  useTypedQuery as _useTypedQuery,
+  useTypedMutation as _useTypedMutation,
+} from 'lib/gql/zeusUser/reactQuery';
 import type { UseQueryOptions } from 'react-query';
+import { UseMutationOptions } from 'react-query';
 
 import { getAuthToken } from '../services/api';
 import { REACT_APP_HASURA_URL } from 'config/env';
@@ -37,6 +41,31 @@ function useTypedQuery<
   );
 }
 
+export function useTypedMutation<
+  O extends 'mutation_root',
+  TData extends ValueTypes[O],
+  TResult = InputType<GraphQLTypes[O], TData>
+>(
+  mutationKey: string | unknown[],
+  mutation: TData | ValueTypes[O],
+  options?: Omit<UseMutationOptions<TResult>, 'mutationKey' | 'mutationFn'>,
+  zeusOptions?: OperationOptions,
+  hostOptions: chainOptions[1] = {}
+) {
+  return _useTypedMutation(
+    mutationKey,
+    mutation,
+    options, // suspense not an option here
+    zeusOptions,
+    REACT_APP_HASURA_URL,
+    {
+      ...hostOptions,
+      method: 'POST',
+      headers: { Authorization: 'Bearer ' + getAuthToken() },
+    }
+  );
+}
+
 export function useCurrentOrg() {
   const id = useSelectedCircle().circle.protocol_id;
 
@@ -49,4 +78,16 @@ export function useCircleIdForEpoch(epochId: number) {
   return useTypedQuery(`circle-for-epoch-${epochId}`, {
     epochs_by_pk: [{ id: epochId }, { circle_id: true }],
   }).data?.epochs_by_pk?.circle_id;
+}
+
+export function useUploadProfileAvatar(image_data_base64: string) {
+  // TODO: i don't know what to do with this key, is it supposed to be unique?
+  return useTypedMutation('update-profile-X', {
+    // not sure why object: is here or needed
+    // TODO: i want this image_data to be passed in when .mutate() is called on this, but I don't know
+    update_profile_avatar: [
+      { object: { image_data: image_data_base64 } },
+      { profile_id: true },
+    ],
+  });
 }
