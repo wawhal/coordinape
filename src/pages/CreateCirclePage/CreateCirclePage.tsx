2a65c32dadecdbd178eccf2df5b334c40f3388c2
CryptoGraffe <graffe@graffe.co>
2022-03-16 22:30:13 -0700

    implement createCircle mutation in FE, remove HASURA_ENABLED flag, make some columns non-nullable also removes HASURA_ENABLED var, deletes unused concepts like FormCaptcha and uxresearchjson

--- src/pages/CreateCirclePage/CreateCirclePage.tsx
+++ src/pages/CreateCirclePage/CreateCirclePage.tsx
@@ -5,12 +5,7 @@ import { useNavigate } from 'react-router-dom';
 
 import { makeStyles, Button } from '@material-ui/core';
 
-import {
-  FormCaptcha,
-  FormTextField,
-  ApeTextField,
-  FormAutocomplete,
-} from 'components';
+import { FormTextField, ApeTextField, FormAutocomplete } from 'components';
 import CreateCircleForm from 'forms/CreateCircleForm';
 import { useApiWithProfile, useApiBase } from 'hooks';
 import { DiscordIcon } from 'icons';
@@ -138,24 +133,9 @@ export const SummonCirclePage = () => {
       <h2 className={classes.title}>Create a Circle</h2>
       <CreateCircleForm.FormController
         source={undefined}
-        submit={async ({
-          captcha_token,
-          research_org_link,
-          research_contact,
-          ...params
-        }) => {
+        submit={async ({ ...params }) => {
           try {
-            const newCircle = await createCircle(
-              myAddress,
-              { ...params },
-              captcha_token,
-              JSON.stringify({
-                address: myAddress,
-                research_org_link,
-                research_contact,
-                ...params,
-              })
-            );
+            const newCircle = await createCircle({ ...params });
             selectCircle(newCircle.id);
             navigate({
               pathname: paths.getAdminPath(),
@@ -217,7 +197,7 @@ export const SummonCirclePage = () => {
             <div className={classes.bodyInner}>
               <div className={classes.twoColumnGrid}>
                 <FormTextField
-                  {...fields.research_contact}
+                  {...fields.contact}
                   fullWidth
                   label="Circle Point of Contact"
                   placeholder="Discord #0000, Telegram, Twitter or Email "
@@ -246,7 +226,6 @@ export const SummonCirclePage = () => {
                 </div>
               </div>
             </div>
-            <FormCaptcha {...fields.captcha_token} error={false} />
             <Button
               className={classes.saveButton}
               variant="contained"
